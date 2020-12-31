3b25b23a75b4f465153e37c8da4a78a257df9e8b
Anton Golosnichenko <golosnichenko@artics.ru>
2020-12-31 09:40:31 +0300

    3.3 Сохраняем имя пользователя - часть 2

--- greetings.js
+++ greetings.js
@@ -4,16 +4,32 @@ const form = document.querySelector('.js-form'),
 const USER_LS = 'currentUsername',
       SHOWING_CN = 'showing';
 
+function saveUsername(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function submitHandler(event) {
+    event.preventDefault();
+    const inputValue = input.value;
+    showGreeting(inputValue);
+    saveUsername(inputValue);
+}
+
 function showGreeting(text) {
     greetings.innerText = `Привет, ${text}`;
     greetings.classList.add(SHOWING_CN);
     form.classList.remove(SHOWING_CN);
 }
 
+function askForUsername(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener('submit', submitHandler);
+}
+
 function loadUsername() {
     const currentUsername = localStorage.getItem(USER_LS);
     if (currentUsername === null) {
-        // если пользователь не найден
+        askForUsername();
     } else {
         showGreeting(currentUsername);
     }
